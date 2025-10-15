4dcfc62206c64f0f4a3f425a3f2d4710e80e9814
stefanolazzari <stefanolazzari79@libero.it>
2025-10-15 07:35:53 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+//dichiariamo una funzione con 2 parametri
+//la dichiarazione si fa una volta
+function saluta(saluto,nome){
+    //alert() è una funzione della libreria standard javascript
+    //ha bisogno di una stringa come parametro 
+    alert(saluto + " " + nome);
+}
+
+//chiamiamo la funzione
+//possiamo chiamarla più volte
+//saluta("Ciao","Pippo");
+//saluta("Hi","John");
+//saluta("Buongiorno","prof");
+
+
+
+function giveMeADado(){
+    let valoreRandomZeroUno = Math.random();
+    let valoreZeroCento = valoreRandomZeroUno * 100;
+    let valoreArrotond = Math.floor(valoreZeroCento);
+    let divRisultato = document.getElementById("risultato");
+    divRisultato.textContent = valoreArrotond;
+}
+
+
+
+
+
+
+
+
+let raggio = 50;
+
+let circonferenza = raggio * 2 * Math.PI;
+
+
+
+
